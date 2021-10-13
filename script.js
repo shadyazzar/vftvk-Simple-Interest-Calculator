7e6e7904da2849d77584b2218b194044b5c6c8a7
shadyazzar <92307669+shadyazzar@users.noreply.github.com>
2021-10-13 19:17:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
-        
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
